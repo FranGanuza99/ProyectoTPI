415cf1cc202c0faa5698278fbdb52b1efc7f2441
FranGanuza99 <franganuza@hotmail.com>
2020-12-23 21:01:32 -0600

    inicio de sesion, registro y recuperacion de contraseña

--- SistemaAudios/models.py
+++ SistemaAudios/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    codigoSeguridad = models.CharField(max_length=20, null = True, blank=True)
+
+class Categoria(models.Model):
+    idCategoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=16)
+    descripcion = models.CharField(max_length=50)
+
+class Recurso(models.Model):
+    idRecurso = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=16)
+    descripcion = models.CharField(max_length=56)
+    archivo = models.CharField(max_length=150)
+    categoria = models.ForeignKey(Categoria, null = True, blank = True, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, null = True, blank = True, on_delete=models.CASCADE)
+
+class Lista(models.Model):
+    idLista = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=56)
+    tipo = models.BooleanField()
+    usuario = models.ForeignKey(User, null = True, blank = True, on_delete=models.CASCADE)
+
+class DetalleLista(models.Model):
+    idDetalle = models.AutoField(primary_key=True)
+    lista = models.ForeignKey(Lista, null = True, blank = True, on_delete=models.CASCADE)
+    recurso = models.ForeignKey(Recurso, null = True, blank = True, on_delete=models.CASCADE)
